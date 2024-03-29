26f0b2526affaa3c5bff003585f894bc7d9f4a96
RiadHossain43 <riadhossain7464@gmail.com>
2024-03-30 00:12:28 +0600

    changes

--- packages/search-api/config/dbConnectionManager.js
+++ packages/search-api/config/dbConnectionManager.js
@@ -1,15 +1,11 @@
 const mongoose = require("mongoose");
 const mongoosePaginate = require("mongoose-paginate-v2");
+const mongooseAggregatePaginate = require("mongoose-aggregate-paginate-v2");
 exports.connectDataBase = async () => {
   try {
     mongoose.plugin(mongoosePaginate);
-    let connection = await mongoose.connect(
-      process.env.MONGO_URI + process.env.DATABASE,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    mongoose.plugin(mongooseAggregatePaginate);
+    await mongoose.connect(process.env.MONGO_URI + process.env.DATABASE, {});
     console.log("Database connected");
   } catch (err) {
     console.log(err);
--- packages/search-api/controllers/search/getResults.js
+++ packages/search-api/controllers/search/getResults.js
@@ -5,12 +5,14 @@ const { formatPagination } = require("../../utils/formats");
 exports.getResults = async (req, res, next) => {
   let { page, sort, size } = req.query;
   const options = { page, limit: size, sort };
-  let filter = new Filters(req.query, { searchFields: ["name"] })
-    .build()
-    .query();
-  let query = { ...filter };
+  // let filter = new Filters(req.query, { searchFields: ["name"] })
+  //   .build()
+  //   .query();
+  // let query = { ...filter };
   try {
-    const queryResults = await Company.paginate(query, options);
+    const pipeline = Company.aggregate();
+    const queryResults = await Company.aggregatePaginate(pipeline, options);
+    console.log(queryResults)
     res.status(200).json({
       message: "Data retrival success.",
       results: queryResults.docs,
